3251880bbc2065f2b591fbb1a3839f1c6237889e
Benjamin V Dev <113056824+Benjamin-V-Dev@users.noreply.github.com>
2023-10-21 19:23:22 +0200

    Amélioration des commentaires, des variables et des fonctions

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,49 +1,58 @@
 /*********************************************************************************
- *
- * Ce fichier contient toutes les fonctions nécessaires au login
- *
+ * Ce fichier gère la logique de connexion de l'utilisateur.
+ * Il écoute l'événement 'submit' sur le formulaire de login,
+ * récupère les données du formulaire,
+ * envoie une requête POST à l'API de login,
+ * et gère les réponses et les erreurs.
  *********************************************************************************/
 
-const url = "http://localhost:5678/api/users/login";
+// URL de l'API pour le login
+const loginApiUrl = "http://localhost:5678/api/users/login";
 
-document.querySelector('#login form').addEventListener('submit', (e)=> {
-    e.preventDefault()
-    let emailInput = document.querySelector('input[type = email]').value;
-    let passwordInput = document.querySelector('input[type = password]').value;
+// Écoute de l'événement 'submit' sur le formulaire de login
+document.querySelector('#login form').addEventListener('submit', (event) => {
+    event.preventDefault();
     
-    const userData = {
-        email: emailInput,
-        password: passwordInput
+    // Récupération des valeurs des champs du formulaire
+    const email = document.querySelector('input[type=email]').value;
+    const password = document.querySelector('input[type=password]').value;
+    
+    // Préparation des données à envoyer
+    const payload = {
+        email,
+        password
     };
     
-    const options = {
+    // Options de la requête fetch
+    const requestOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(userData)
+        body: JSON.stringify(payload)
     };
     
-    fetch(url, options)
+    // Envoi de la requête
+    fetch(loginApiUrl, requestOptions)
     .then(response => {
         if (!response.ok) {
-            throw new Error("Erreur réseau ou côté serveur");
+            throw new Error("Erreur lors de la connexion. Vérifiez vos identifiants.");
         }
         return response.json();
     })
     .then(data => {
-        const token = data.token;
+        const { token } = data;
         console.log("Token reçu:", token);
-        window.location.href = "./index.html"
+        
+        // TODO: Utiliser une méthode plus sécurisée pour stocker le token
         localStorage.setItem("userToken", token);
-        localStorage.setItem("accessToken", "true")
+        localStorage.setItem("accessToken", "true");
+        
+        // Redirection vers la page d'accueil
+        window.location.href = "./index.html";
     })
     .catch(error => {
         console.error("Une erreur s'est produite:", error.message);
-        document.querySelector(".errorLogin").textContent = "Woups, erreur d'identifiants ?" 
-
+        document.querySelector(".errorLogin").textContent = "Erreur lors de la connexion. Vérifiez vos identifiants.";
     });
-
-    
 });
-
